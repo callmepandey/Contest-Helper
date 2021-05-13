71f76b9c7a980908cca8a0e6482b6d9b76067635
callmepandey <42908589+callmepandey@users.noreply.github.com>
2021-05-13 11:06:14 +0530

    Added Bootstrap with better UI now

--- ContestHelp/settings.py
+++ ContestHelp/settings.py
@@ -9,13 +9,13 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
+STATIC_DIR = os.path.join(BASE_DIR,'static')
+TEMPLATES_DIR = os.path.join(BASE_DIR,'templates')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'ContestHelp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -67,7 +67,8 @@ TEMPLATES = [
         },
     },
 ]
-
+STATIC_URL = '/static/'
+STATICFILES_DIRS=[STATIC_DIR]
 WSGI_APPLICATION = 'ContestHelp.wsgi.application'
 
 
