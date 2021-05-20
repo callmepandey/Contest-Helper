9c18f02a64a7bc74681eaf9121167d99ceb9f97e
callmepandey <42908589+callmepandey@users.noreply.github.com>
2021-05-20 16:47:58 +0530

    Static Files Included bug

--- ContestHelp/urls.py
+++ ContestHelp/urls.py
@@ -16,12 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
-from django.conf import settings
-from django.conf.urls.i18n import i18n_patterns
-from django.conf.urls.static import static
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = i18n_patterns(path('admin/', admin.site.urls), path('' , include('Contest.urls')), prefix_default_language=False) + \
-    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + \
-    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('Contest.urls'))
+]
--- ContestHelp/wsgi.py
+++ ContestHelp/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-
+from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ContestHelp.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root='/path/to/static/files')
+application.add_files('/path/to/more/static/files', prefix='more-files/')
